f85b42f79749ae928f6b502ec827dde3682510cc
Linux-Scratcher <contact@linux-scratcher.fr>
2025-01-29 09:53:43 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# Faire fonctionner pacman sur votre machine Steam :
 sudo pacman-key --init
 sudo pacman-mirrors --fasttrack 5
 sudo pacman -Syyu
+
+# Mode bureau plasma au démarage de la machine sans passer par le steam big picture :
+steamos-session-select plasma-persistent
