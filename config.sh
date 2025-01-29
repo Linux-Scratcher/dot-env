cf50df3070d941118bade5373d252d1b261d5b5a
Linux-Scratcher <contact@linux-scratcher.fr>
2025-01-29 09:57:18 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-
 # Faire fonctionner pacman sur votre machine Steam :
+sudo steamos-readonly disable; sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman -Sy --noconfirm conky
+sudo pacman-key --populate holo
 sudo pacman-key --init
-sudo pacman-mirrors --fasttrack 5
-sudo pacman -Syyu
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
 
 # Mode bureau plasma au démarage de la machine sans passer par le steam big picture :
 steamos-session-select plasma-persistent
